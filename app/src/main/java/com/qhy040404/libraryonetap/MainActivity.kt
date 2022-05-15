a11ec6ddfde01b9f5f81248e33b23dee17afe0e5
qhy040404 <qhy040404@163.com>
2022-05-15 09:16:14 +0800

    Close #36

--- app/src/main/java/com/qhy040404/libraryonetap/MainActivity.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/MainActivity.kt
@@ -2,7 +2,9 @@ package com.qhy040404.libraryonetap
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.qhy040404.libraryonetap.tools.ToolsInitActivity
 import kotlin.system.exitProcess
@@ -11,6 +13,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        Thread(ShowVersion()).start()
+    }
+
+    private inner class ShowVersion:Runnable {
+        override fun run() {
+            Looper.prepare()
+
+            val versionView:TextView = findViewById(R.id.textView5)
+
+            val packManager = packageManager
+            val packInfo = packManager.getPackageInfo(packageName,0)
+            val versionCode = packInfo.longVersionCode
+            val versionName = packInfo.versionName
+
+            val version = getString(R.string.app_name) + " " + versionName + "($versionCode)"
+
+            versionView.text = version
+            Looper.loop()
+        }
     }
 
     fun buttonOpenDetail(view: View) {

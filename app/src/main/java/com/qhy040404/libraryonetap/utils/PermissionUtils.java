d2309102c726d746d52693407dbeaaf674c77bd7
qhy040404 <qhy040404@163.com>
2022-05-16 00:07:19 +0800

    PermissionUtils and String consts

---
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/PermissionUtils.java
@@ -0,0 +1,30 @@
+package com.qhy040404.libraryonetap.utils;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import com.qhy040404.libraryonetap.R;
+
+public class PermissionUtils {
+    public boolean checkPermission(Activity activity, String[] permission) {
+        boolean hasPermission = true;
+        int j = 0;
+        for (String s : permission) {
+            if (activity.checkSelfPermission(s) == PackageManager.PERMISSION_DENIED) {
+                if (s != null) {
+                    hasPermission = false;
+                    requestPermission(new String[]{s}, activity);
+                }
+            }
+        }
+        return hasPermission;
+    }
+
+    private static void requestPermission(String[] s, Activity activity) {
+        Toast.makeText(activity, R.string.promptPermission, Toast.LENGTH_SHORT).show();
+        ActivityCompat.requestPermissions(activity, s, 100);
+    }
+}

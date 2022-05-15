3f9f0ba4d7899793e239a2909d6adda17515f662
qhy040404 <qhy040404@163.com>
2022-05-16 00:06:57 +0800

    NetworkStateUtils.kt

---
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/NetworkStateUtils.kt
@@ -0,0 +1,34 @@
+package com.qhy040404.libraryonetap.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.telephony.TelephonyManager
+import androidx.core.content.ContextCompat
+
+class NetworkStateUtils {
+    private fun checkNetworkType(ctx: Context): Int {
+        val context = ctx.applicationContext
+        val netType = 0
+        val manager = ContextCompat.getSystemService(context, ConnectivityManager::class.java)
+            ?: return netType
+        val networkInfo = manager.activeNetworkInfo ?: return netType
+        return when (networkInfo.type) {
+            ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_WIMAX, ConnectivityManager.TYPE_ETHERNET -> 1
+            ConnectivityManager.TYPE_MOBILE -> when (networkInfo.subtype) {
+                TelephonyManager.NETWORK_TYPE_LTE, TelephonyManager.NETWORK_TYPE_HSPAP, TelephonyManager.NETWORK_TYPE_EHRPD -> 2
+                TelephonyManager.NETWORK_TYPE_UMTS, TelephonyManager.NETWORK_TYPE_CDMA, TelephonyManager.NETWORK_TYPE_EVDO_0, TelephonyManager.NETWORK_TYPE_EVDO_A, TelephonyManager.NETWORK_TYPE_EVDO_B -> 2
+                TelephonyManager.NETWORK_TYPE_GPRS, TelephonyManager.NETWORK_TYPE_EDGE -> 2
+                else -> netType
+            }
+            else -> netType
+        }
+    }
+
+    fun checkNetworkTypeStr(ctx: Context): String {
+        return when (checkNetworkType(ctx)) {
+            1 -> "WIFI"
+            2 -> "Cellular"
+            else -> "Error"
+        }
+    }
+}

3f5e5c85af5688de85ef31fa84aa262669dca2a1
qhy040404 <qhy040404@163.com>
2022-05-17 11:10:12 +0800

    Fix bug

--- app/src/main/java/com/qhy040404/libraryonetap/activity/SettingsActivity.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/activity/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.qhy040404.libraryonetap.activity
 
 import android.content.Intent
 import android.view.KeyEvent
+import android.view.MenuItem
 import com.qhy040404.libraryonetap.R
 import com.qhy040404.libraryonetap.fragment.SettingsFragment
 
@@ -22,5 +23,11 @@ class SettingsActivity : StartUpActivity() {
             finish()
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }

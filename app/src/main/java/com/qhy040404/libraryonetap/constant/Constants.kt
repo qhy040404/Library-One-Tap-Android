be6fb42c31b3c9f1d9a270db1d3239caea47d32d
qhy040404 <qhy040404@163.com>
2022-05-17 13:19:58 +0800

    Ready for shortcuts

--- app/src/main/java/com/qhy040404/libraryonetap/constant/Constants.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/constant/Constants.kt
@@ -17,4 +17,8 @@ object Constants {
     const val CONTENT_TYPE_SSO = "application/x-www-form-urlencoded; charset=utf-8"
     const val CONTENT_TYPE_JSON = "application/json;charset=UTF-8"
     const val CONTENT_TYPE_VCARD = "application/x-www-form-urlencoded"
+
+    const val SHORTCUT_DETAIL = "com.qhy040404.libraryonetap.intent.action.DETAIL"
+    const val SHORTCUT_BATH = "com.qhy040404.libraryonetap.intent.action.BATH"
+    const val SHORTCUT_VCARD = "com.qhy040404.libraryonetap.intent.action.VCARD"
 }

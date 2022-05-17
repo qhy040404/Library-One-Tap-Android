a6bbbfbd134f5079f02651db36c807fd33a571be
qhy040404 <qhy040404@163.com>
2022-05-17 09:57:47 +0800

    Unify format

--- app/src/main/java/com/qhy040404/libraryonetap/activity/SettingsActivity.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/activity/SettingsActivity.kt
@@ -1,44 +1,20 @@
 package com.qhy040404.libraryonetap.activity
 
 import android.content.Intent
-import android.os.Bundle
 import android.view.KeyEvent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.preference.PreferenceFragmentCompat
 import com.qhy040404.libraryonetap.R
-import com.qhy040404.libraryonetap.constant.Constants
-import rikka.preference.SimpleMenuPreference
+import com.qhy040404.libraryonetap.fragment.SettingsFragment
 
-class SettingsActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        val sp = getSharedPreferences(
-            "com.qhy040404.libraryonetap_preferences",
-            MODE_PRIVATE
-        )
-        when (sp.getString("dark", "system").toString()) {
-            "system" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            "on" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            "off" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        }
-        when (sp.getString("theme", "purple").toString()) {
-            "purple" -> setTheme(R.style.Theme_Purple)
-            "blue" -> setTheme(R.style.Theme_Blue)
-            "pink" -> setTheme(R.style.Theme_Pink)
-            "green" -> setTheme(R.style.Theme_Green)
-            "simple" -> setTheme(R.style.Theme_Simple)
-        }
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
+class SettingsActivity : StartUpActivity() {
+    override fun init() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun getLayoutId(): Int = R.layout.settings_activity
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -46,25 +22,5 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
         return super.onKeyDown(keyCode, event)
-    }
-
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
-            findPreference<SimpleMenuPreference>(Constants.PREF_DARK)?.apply {
-                setOnPreferenceChangeListener { _, _ ->
-                    activity?.recreate()
-                    true
-                }
-            }
-
-            findPreference<SimpleMenuPreference>(Constants.PREF_THEME)?.apply {
-                setOnPreferenceChangeListener { _, _ ->
-                    activity?.recreate()
-                    true
-                }
-            }
-        }
     }
 }

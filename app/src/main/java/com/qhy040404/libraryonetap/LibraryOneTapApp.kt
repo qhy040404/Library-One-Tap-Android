3630431ada74b5245004bf3bf0cf3cbb972b7fa0
qhy040404 <qhy040404@163.com>
2022-05-17 23:16:57 +0800

    Complete bugly(maybe)

--- app/src/main/java/com/qhy040404/libraryonetap/LibraryOneTapApp.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/LibraryOneTapApp.kt
@@ -2,6 +2,12 @@ package com.qhy040404.libraryonetap
 
 import android.app.Activity
 import android.app.Application
+import android.os.Environment
+import com.qhy040404.libraryonetap.activity.MainActivity
+import com.qhy040404.libraryonetap.constant.Constants
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.BuglyStrategy
+import com.tencent.bugly.beta.Beta
 import java.util.*
 
 class LibraryOneTapApp : Application() {
@@ -9,7 +15,25 @@ class LibraryOneTapApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        setBeta()
+
+        Bugly.init(applicationContext, Constants.BUGLY_APPID,true)
         app = this
+    }
+
+    private fun setBeta() {
+        Beta.autoInit=true
+        Beta.autoCheckUpgrade=true
+        Beta.upgradeCheckPeriod = 0*1000
+        Beta.initDelay = 1*1000
+        Beta.largeIconId=R.mipmap.launcher_lol
+        Beta.smallIconId=R.mipmap.launcher_lol
+        Beta.defaultBannerId=R.mipmap.launcher_lol
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        Beta.showInterruptedStrategy=true
+        Beta.canShowUpgradeActs.add(MainActivity::class.java)
+        Beta.enableHotfix=false
     }
 
     fun addActivity(activity: Activity) {

50e3fea6a4a58a343449813461b7dbc8dd253c12
qhy040404 <qhy040404@163.com>
2022-05-27 23:24:29 +0800

    Add const

--- app/src/main/java/com/qhy040404/libraryonetap/constant/URLManager.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/constant/URLManager.kt
@@ -13,6 +13,7 @@ object URLManager {
     const val LIBRARY_ORDER_CANCEL_URL =
         "http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=myOrderOperation"
     const val LIBRARY_QR_CERT_URL = "http://seat.lib.dlut.edu.cn/yanxiujian/client/2codecert.php?"
+    const val LIBRARY_RESERVE_URL = "http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=randomRoomSeatChoose"
 
     const val PORTAL_SSO_URL =
         "https://sso.dlut.edu.cn/cas/login?service=https%3A%2F%2Fportal.dlut.edu.cn%2Ftp%2Fview%3Fm%3Dup#act=portal/viewhome"

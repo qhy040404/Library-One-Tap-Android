9952a0df7aa1531440f1810f7ad09e4d8fd3cff9
qhy040404 <qhy040404@163.com>
2022-05-31 00:10:51 +0800

    Ready for `reset`

--- app/src/main/java/com/qhy040404/libraryonetap/utils/ReserveUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/ReserveUtils.kt
@@ -1,16 +1,25 @@
 package com.qhy040404.libraryonetap.utils
 
-import java.util.*
-
 object ReserveUtils {
+    fun getResetRoomCode(space_name:String):Int {
+        val area = space_name.split("图书馆")[0]
+        val room = space_name.split("图书馆")[1].split("阅")[0]
+        return RoomUtils.getRoomCode(area,room)
+    }
+
     fun constructPara(room: Int): String {
-        val calendar = Calendar.getInstance()
-        val today =
-            calendar[Calendar.YEAR].toString() + "/" + (calendar[Calendar.MONTH] + 1).toString() + "/" + calendar[Calendar.DAY_OF_MONTH].toString()
-        return "room_id=$room&order_date=$today"
+        return "room_id=$room&order_date=${getToday()}"
+    }
+
+    fun constructParaForAddCode(seat_id:String) :String {
+        return "seat_id=$seat_id&order_date=${getToday()}"
     }
 
     fun constructParaForFinalReserve(addCode: String): String {
         return "addCode=$addCode&method=addSeat"
+    }
+
+    fun formatAvailableMap(am:String):String {
+        return am.replace("\\ufeff\\r\\n\\r\\n[[","").replace("]]\\r\\n\\r\\n\\r\\n\\r\\n","").replace("{","").replace("}","")
     }
 }

530888b5d64c3e22cc93af056a89d13ab8907933
qhy040404 <qhy040404@163.com>
2022-05-31 00:10:04 +0800

    Standardization calendar

--- app/src/main/java/com/qhy040404/libraryonetap/utils/TimeUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/TimeUtils.kt
@@ -1,5 +1,13 @@
 package com.qhy040404.libraryonetap.utils
 
+import java.util.*
+
+val calendar: Calendar = Calendar.getInstance()
+
+fun getToday():String {
+    return calendar[Calendar.YEAR].toString() + "/" + (calendar[Calendar.MONTH] + 1).toString() + "/" + calendar[Calendar.DAY_OF_MONTH].toString()
+}
+
 fun timeSingleToDouble(sTime: Int): String {
     val resultTime: String = if (sTime >= 10) {
         sTime.toString()

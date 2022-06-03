da5940b3755318120fae4c0ae0f1e44c1b6f6066
qhy040404 <qhy040404@163.com>
2022-06-03 13:50:40 +0800

    Add ability to show `Release` or `Debug`

--- app/src/main/java/com/qhy040404/libraryonetap/utils/PackageUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/PackageUtils.kt
@@ -1,11 +1,18 @@
 package com.qhy040404.libraryonetap.utils
 
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import com.qhy040404.libraryonetap.LibraryOneTapApp
+
 
 object PackageUtils {
     private val packInfo: PackageInfo =
         LibraryOneTapApp.app.packageManager.getPackageInfo(LibraryOneTapApp.app.packageName, 0)
+    private val appInfo: ApplicationInfo = LibraryOneTapApp.app.packageManager.getApplicationInfo(LibraryOneTapApp.app.packageName, PackageManager.GET_META_DATA)
+
     val versionCode = packInfo.longVersionCode
     val versionName: String = packInfo.versionName
+
+    val buildType:String = appInfo.metaData.getString("channel").toString()
 }

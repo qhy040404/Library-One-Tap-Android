4b8e3848ade95a3751dd04ca2a1a8a128362dc5c
qhy040404 <qhy040404@163.com>
2022-06-11 18:16:42 +0800

    Use new method to get network state

--- app/src/main/java/com/qhy040404/libraryonetap/utils/tools/NetworkStateUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/tools/NetworkStateUtils.kt
@@ -2,8 +2,8 @@ package com.qhy040404.libraryonetap.utils.tools
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.wifi.WifiManager
-import android.telephony.TelephonyManager
 import androidx.core.content.ContextCompat
 
 @Suppress("DEPRECATION")
@@ -13,15 +13,11 @@ class NetworkStateUtils {
         val netType = 0
         val manager = ContextCompat.getSystemService(context, ConnectivityManager::class.java)
             ?: return netType
-        val networkInfo = manager.activeNetworkInfo ?: return netType
-        return when (networkInfo.type) {
-            ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_WIMAX, ConnectivityManager.TYPE_ETHERNET -> 1
-            ConnectivityManager.TYPE_MOBILE -> when (networkInfo.subtype) {
-                TelephonyManager.NETWORK_TYPE_LTE, TelephonyManager.NETWORK_TYPE_HSPAP, TelephonyManager.NETWORK_TYPE_EHRPD -> 2
-                TelephonyManager.NETWORK_TYPE_UMTS, TelephonyManager.NETWORK_TYPE_CDMA, TelephonyManager.NETWORK_TYPE_EVDO_0, TelephonyManager.NETWORK_TYPE_EVDO_A, TelephonyManager.NETWORK_TYPE_EVDO_B -> 2
-                TelephonyManager.NETWORK_TYPE_GPRS, TelephonyManager.NETWORK_TYPE_EDGE -> 2
-                else -> netType
-            }
+        val networkCapabilities =
+            manager.getNetworkCapabilities(manager.activeNetwork) ?: return netType
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> 1
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> 2
             else -> netType
         }
     }

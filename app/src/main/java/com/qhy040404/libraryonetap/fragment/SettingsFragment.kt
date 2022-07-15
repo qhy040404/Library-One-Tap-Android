51fc73a8b79493f1456a844f9c5e0ec04a884b34
qhy040404 <qhy040404@163.com>
2022-07-15 15:40:52 +0800

    Fix invisible part

--- app/src/main/java/com/qhy040404/libraryonetap/fragment/SettingsFragment.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/fragment/SettingsFragment.kt
@@ -2,8 +2,14 @@ package com.qhy040404.libraryonetap.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.absinthe.libraries.utils.extensions.addPaddingTop
+import com.absinthe.libraries.utils.utils.UiUtils
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.qhy040404.libraryonetap.LibraryOneTapApp
 import com.qhy040404.libraryonetap.R
@@ -15,10 +21,18 @@ import com.qhy040404.libraryonetap.utils.SPUtils
 import rikka.material.app.DayNightDelegate
 import rikka.material.app.LocaleDelegate
 import rikka.preference.SimpleMenuPreference
+import rikka.recyclerview.fixEdgeEffect
+import rikka.widget.borderview.BorderRecyclerView
+import rikka.widget.borderview.BorderView
+import rikka.widget.borderview.BorderViewDelegate
 import java.util.*
 import kotlin.system.exitProcess
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), IListController {
+
+    private lateinit var borderViewDelegate: BorderViewDelegate
+    private lateinit var prefRecyclerView: RecyclerView
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
@@ -78,4 +92,57 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        scheduleAppbarRaisingStatus(
+            !getBorderViewDelegate().isShowingTopBorder,
+            "SettingsFragment onResume"
+        )
+        (activity as? IAppBarContainer)?.setLiftOnScrollTargetView(prefRecyclerView)
+    }
+
+    override fun onCreateRecyclerView(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        savedInstanceState: Bundle?,
+    ): RecyclerView {
+        val recyclerView =
+            super.onCreateRecyclerView(inflater, parent, savedInstanceState) as BorderRecyclerView
+        recyclerView.id = android.R.id.list
+        recyclerView.fixEdgeEffect()
+        recyclerView.addPaddingTop(UiUtils.getStatusBarHeight())
+        recyclerView.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        recyclerView.isVerticalScrollBarEnabled = false
+
+        val lp = recyclerView.layoutParams
+        if (lp is FrameLayout.LayoutParams) {
+            lp.rightMargin =
+                recyclerView.context.resources.getDimension(rikka.material.R.dimen.rd_activity_horizontal_margin)
+                    .toInt()
+            lp.leftMargin = lp.rightMargin
+        }
+
+        borderViewDelegate = recyclerView.borderViewDelegate
+        borderViewDelegate.borderVisibilityChangedListener =
+            BorderView.OnBorderVisibilityChangedListener { top: Boolean, _: Boolean, _: Boolean, _: Boolean ->
+                scheduleAppbarRaisingStatus(
+                    !top,
+                    "SettingsFragment OnBorderVisibilityChangedListener: top=$top"
+                )
+            }
+
+        prefRecyclerView = recyclerView
+        return recyclerView
+    }
+
+    private fun scheduleAppbarRaisingStatus(isLifted: Boolean, from: String) {
+        (activity as? IAppBarContainer)?.scheduleAppbarLiftingStatus(isLifted, from)
+    }
+
+    override fun onReturnTop() {}
+
+    override fun getBorderViewDelegate(): BorderViewDelegate = borderViewDelegate
+    override fun isAllowRefreshing(): Boolean = true
+    override fun getSuitableLayoutManager(): RecyclerView.LayoutManager? = null
 }

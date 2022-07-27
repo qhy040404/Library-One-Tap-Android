cefac5903c6ff8976702f7c174ff5ad951abe7ed
qhy040404 <qhy040404@163.com>
2022-07-27 09:48:09 +0800

    Catch Exception in GET and POST

--- app/src/main/java/com/qhy040404/libraryonetap/utils/web/Requests.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/web/Requests.kt
@@ -3,7 +3,6 @@ package com.qhy040404.libraryonetap.utils.web
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 @Suppress("SpellCheckingInspection")
@@ -30,26 +29,28 @@ object Requests {
         return ori.toMediaType()
     }
 
-    @Throws(IOException::class)
     fun get(url: String): String {
         return get(url, false)
     }
 
-    @Throws(IOException::class)
     fun get(url: String, getUrl: Boolean): String {
         val request = Request.Builder()
             .url(url)
             .get()
             .build()
-        client.newCall(request).execute().use { response ->
-            if (getUrl) {
-                return response.request.url.toString()
+        try {
+            client.newCall(request).execute().use { response ->
+                if (getUrl) {
+                    return response.request.url.toString()
+                }
+                return response.body!!.string()
             }
-            return response.body!!.string()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
         }
     }
 
-    @Throws(IOException::class)
     fun getVCard(url: String): String {
         val request = Request.Builder()
             .url(url)
@@ -57,30 +58,38 @@ object Requests {
             .addHeader("User-Agent", "weishao")
             .get()
             .build()
-        client.newCall(request).execute().use { response -> return response.body!!.string() }
+        try {
+            client.newCall(request).execute()
+                .use { response -> return response.body!!.string() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
+        }
     }
 
-    @Throws(IOException::class)
     fun post(url: String, form: String, FORM: MediaType): String {
         return post(url, form, FORM, false)
     }
 
-    @Throws(IOException::class)
     fun post(url: String, form: String, FORM: MediaType, getUrl: Boolean): String {
         val body = form.toRequestBody(FORM)
         val request = Request.Builder()
             .url(url)
             .post(body)
             .build()
-        client.newCall(request).execute().use { response ->
-            if (getUrl) {
-                return response.request.url.toString()
+        try {
+            client.newCall(request).execute().use { response ->
+                if (getUrl) {
+                    return response.request.url.toString()
+                }
+                return response.body!!.string()
             }
-            return response.body!!.string()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
         }
     }
 
-    @Throws(IOException::class)
     fun postVCard(url: String, form: String, FORM: MediaType): String {
         val body = form.toRequestBody(FORM)
         val request = Request.Builder()
@@ -89,7 +98,13 @@ object Requests {
             .addHeader("User-Agent", "weishao")
             .post(body)
             .build()
-        client.newCall(request).execute().use { response -> return response.body!!.string() }
+        try {
+            client.newCall(request).execute()
+                .use { response -> return response.body!!.string() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
+        }
     }
 
     fun loginPostData(

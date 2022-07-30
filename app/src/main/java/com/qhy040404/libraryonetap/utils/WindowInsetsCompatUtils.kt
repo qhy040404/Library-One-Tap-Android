16390c603e0c7f02d96209d93ea9e5be18068771
qhy040404 <qhy040404@163.com>
2022-07-30 20:22:45 +0800

    Resolve some warnings

---
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/WindowInsetsCompatUtils.kt
@@ -0,0 +1,26 @@
+package com.qhy040404.libraryonetap.utils
+
+import androidx.core.view.WindowInsetsCompat
+
+@Suppress("DEPRECATION")
+object WindowInsetsCompatUtils {
+    fun getInsetsParam(windowInsets: WindowInsetsCompat, typeMask: Int, param: String): Int {
+        return if (OsUtils.atLeastR()) {
+            when (param) {
+                "left" -> windowInsets.getInsets(typeMask).left
+                "right" -> windowInsets.getInsets(typeMask).right
+                "top" -> windowInsets.getInsets(typeMask).top
+                "bottom" -> windowInsets.getInsets(typeMask).bottom
+                else -> throw NullPointerException("invalid param")
+            }
+        } else {
+            when (param) {
+                "left" -> windowInsets.systemWindowInsetLeft
+                "right" -> windowInsets.systemWindowInsetRight
+                "top" -> windowInsets.systemWindowInsetTop
+                "bottom" -> windowInsets.systemWindowInsetBottom
+                else -> throw NullPointerException("invalid param")
+            }
+        }
+    }
+}

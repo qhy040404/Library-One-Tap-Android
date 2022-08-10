5b7535950e3ace23e1b1eaab7c04eb33eb7a8701
qhy040404 <qhy040404@163.com>
2022-08-10 20:54:25 +0800

    Change a method to globally dismiss DialogFragment

--- app/src/main/java/com/qhy040404/libraryonetap/ui/fragment/fullscreen/FullScreenDialogFragment.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/ui/fragment/fullscreen/FullScreenDialogFragment.kt
@@ -1,12 +1,19 @@
 package com.qhy040404.libraryonetap.ui.fragment.fullscreen
 
+import android.os.Bundle
 import android.view.Gravity
 import android.view.ViewGroup
+import com.qhy040404.libraryonetap.LibraryOneTapApp
 import com.qhy040404.libraryonetap.base.BaseBindingDialogFragment
 import com.qhy040404.libraryonetap.databinding.FragmentFullscreenDialogBinding
 
 class FullScreenDialogFragment(private val message: String) :
     BaseBindingDialogFragment<FragmentFullscreenDialogBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LibraryOneTapApp.instance?.addFragment(this)
+    }
+
     override fun onResume() {
         super.onResume()
         dialog?.window?.run {

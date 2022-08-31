43bfa82d94d0d99fdad0192a872320da1f7feaa0
qhy040404 <qhy040404@163.com>
2022-08-31 10:09:17 +0800

    Simplify code

--- app/src/main/java/com/qhy040404/libraryonetap/utils/web/CookieJarImpl.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/web/CookieJarImpl.kt
@@ -4,17 +4,15 @@ import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
 
-object CookieJarImpl {
+object CookieJarImpl : CookieJar {
     private val cookieStore = mutableListOf<Cookie>()
 
-    object OkCookieJar : CookieJar {
-        override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
-            cookieStore.addAll(cookies)
-        }
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        cookieStore.addAll(cookies)
+    }
 
-        override fun loadForRequest(url: HttpUrl): List<Cookie> {
-            return cookieStore
-        }
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        return cookieStore
     }
 
     fun getCookies(): List<Cookie> {

c2947e3a7fd5f6d259803677a6b3068388d755c3
qhy040404 <qhy040404@163.com>
2022-09-07 11:17:56 +0800

    Compatibility

--- app/src/main/java/com/qhy040404/libraryonetap/ui/main/MainActivity.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/ui/main/MainActivity.kt
@@ -187,6 +187,10 @@ class MainActivity : BaseActivity<ActivityMainBottomBinding>(),
 
     private fun showWelcomeDialog() {
         if (!Once.beenDone(Once.THIS_APP_INSTALL, OnceTag.FIRST_DATA_INPUT)) {
+            if (GlobalValues.initialized) {
+                Once.markDone(OnceTag.FIRST_DATA_INPUT)
+                return
+            }
             MaterialAlertDialogBuilder(this)
                 .setTitle(R.string.welcome)
                 .setMessage(R.string.welcome_message)

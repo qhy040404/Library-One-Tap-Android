59dadc5950168d3d064f4139fbeb4fca21b12dc4
qhy040404 <qhy040404@163.com>
2022-09-07 20:11:21 +0800

    Add more unit test

--- app/src/test/java/com/qhy040404/libraryonetap/DesTest.kt
+++ app/src/test/java/com/qhy040404/libraryonetap/DesTest.kt
@@ -8,7 +8,13 @@ class DesTest {
     @Test
     fun testEncAndDec() {
         val des = DesEncryptUtils()
-        val testString = "ThisIsATest"
-        assertEquals(testString, des.strDec(des.strEnc(testString, "1", "2", "3"), "1", "2", "3"))
+        val testStringA = "ThisIsATest"
+        val testStringB = "AnotherKeyTest"
+        val testStringC = "LongKeyTest"
+        assertEquals(testStringA, des.strDec(des.strEnc(testStringA, "1", "2", "3"), "1", "2", "3"))
+        assertEquals(testStringB,
+            des.strDec(des.strEnc(testStringB, "a", "b", "cc"), "a", "b", "cc"))
+        assertEquals(testStringC,
+            des.strDec(des.strEnc(testStringC, "a", "little", "longer"), "a", "little", "longer"))
     }
 }

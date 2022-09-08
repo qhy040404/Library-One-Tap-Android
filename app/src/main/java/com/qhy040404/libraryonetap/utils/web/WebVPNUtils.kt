1273ac05c2a089983d0c7de6570c4c00b5ac3df0
qhy040404 <qhy040404@163.com>
2022-09-08 08:18:05 +0800

    [skip ci]Suppress unused

--- app/src/main/java/com/qhy040404/libraryonetap/utils/web/WebVPNUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/web/WebVPNUtils.kt
@@ -3,6 +3,7 @@ package com.qhy040404.libraryonetap.utils.web
 import com.qhy040404.libraryonetap.annotation.HttpProtocols
 import com.qhy040404.libraryonetap.runner.JsRunner
 
+@Suppress("unused") // Remove when call encrypt()
 object WebVPNUtils {
     private const val institution = "https://webvpn.dlut.edu.cn"
 

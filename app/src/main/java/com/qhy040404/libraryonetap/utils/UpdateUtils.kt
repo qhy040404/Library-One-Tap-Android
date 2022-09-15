3b3815ae35c9ba0ca280d3a4f1704de5c9229b7a
qhy040404 <qhy040404@163.com>
2022-09-15 20:28:09 +0800

    APK install function init

---
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/UpdateUtils.kt
@@ -0,0 +1,24 @@
+package com.qhy040404.libraryonetap.utils
+
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.FileProvider
+import java.io.File
+
+object UpdateUtils {
+    fun checkUpdate() {
+    }
+
+    fun installApk(ctx: Context) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.setDataAndType(
+            FileProvider.getUriForFile(ctx,
+                "com.qhy040404.libraryonetap.fileprovider",
+                File(ctx.cacheDir, "test.apk")),
+            "application/vnd.android.package-archive")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        ctx.startActivity(intent)
+    }
+}

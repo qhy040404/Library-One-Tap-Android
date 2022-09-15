ec172d464dc527cca0dfb2a220aed8997b0cc84f
qhy040404 <qhy040404@163.com>
2022-09-16 07:57:14 +0800

    [skip ci]Misc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
+    val agpVersion = "7.3.0"
+
+    id("com.android.application") version agpVersion apply false
+    id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 

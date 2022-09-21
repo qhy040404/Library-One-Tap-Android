2cca5d067f733135f8aeab379c5fcf21cc931a1b
qhy040404 <qhy040404@163.com>
2022-09-21 09:19:49 +0800

    Use Okio

--- app/src/main/java/com/qhy040404/libraryonetap/utils/encrypt/AESEncryptUtils.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/encrypt/AESEncryptUtils.kt
@@ -4,7 +4,8 @@ import androidx.annotation.RawRes
 import com.qhy040404.libraryonetap.LibraryOneTapApp
 import com.qhy040404.libraryonetap.R
 import com.qhy040404.libraryonetap.runner.JsRunner
-import org.apache.http.util.EncodingUtils
+import okio.buffer
+import okio.source
 
 object AESEncryptUtils {
     var initialized = false
@@ -16,10 +17,7 @@ object AESEncryptUtils {
 
     private fun initRaw(@RawRes id: Int) {
         LibraryOneTapApp.app.resources.openRawResource(id).apply {
-            val length = available()
-            val buffer = ByteArray(length)
-            read(buffer)
-            JsRunner.initScript(EncodingUtils.getString(buffer, "utf-8"))
+            JsRunner.initScript(source().buffer().readString(Charsets.UTF_8))
         }
     }
 }

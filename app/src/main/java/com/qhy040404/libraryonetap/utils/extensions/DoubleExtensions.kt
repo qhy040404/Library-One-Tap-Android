6a876c0ab3e3871037676fae46d6f06f32d3cbdc
qhy040404 <qhy040404@163.com>
2022-12-12 13:34:39 +0800

    Use BigDecimal.valueOf

--- app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/DoubleExtensions.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/DoubleExtensions.kt
@@ -10,5 +10,5 @@ object DoubleExtensions {
      *
      * @return Double with two decimal
      */
-    fun Double.to2Decimal() = BigDecimal(this).setScale(2, RoundingMode.HALF_UP).toDouble()
+    fun Double.to2Decimal() = BigDecimal.valueOf(this).setScale(2, RoundingMode.HALF_UP).toDouble()
 }

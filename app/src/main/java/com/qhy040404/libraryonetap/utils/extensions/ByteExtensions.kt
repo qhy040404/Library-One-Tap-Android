8a928af9cd9914d2bb4ed31e22f092c82d4b9af6
qhy040404 <qhy040404@163.com>
2022-12-18 10:22:53 +0800

    Add docs

--- app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/ByteExtensions.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/ByteExtensions.kt
@@ -4,11 +4,31 @@ import com.qhy040404.libraryonetap.utils.HashUtils
 
 @Suppress("unused")
 object ByteExtensions {
+    /**
+     * Calculate a ByteArray's MD5 hash
+     *
+     * @return MD5 hash of the ByteArray
+     */
     fun ByteArray.md5() = HashUtils.md5(this)
 
+    /**
+     * Calculate a ByteArray's SHA-1 hash
+     *
+     * @return SHA-1 hash of the ByteArray
+     */
     fun ByteArray.sha1() = HashUtils.sha1(this)
 
+    /**
+     * Calculate a ByteArray's SHA-256 hash
+     *
+     * @return SHA-256 hash of the ByteArray
+     */
     fun ByteArray.sha256() = HashUtils.sha256(this)
 
+    /**
+     * Calculate a ByteArray's SHA-512 hash
+     *
+     * @return SHA-512 hash of the ByteArray
+     */
     fun ByteArray.sha512() = HashUtils.sha512(this)
 }
--- app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/FileExtensions.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/extensions/FileExtensions.kt
@@ -5,11 +5,31 @@ import java.io.File
 
 @Suppress("unused")
 object FileExtensions {
+    /**
+     * Calculate a File's MD5 hash
+     *
+     * @return MD5 hash of the File
+     */
     fun File.md5() = HashUtils.md5(this.readBytes())
 
+    /**
+     * Calculate a File's SHA-1 hash
+     *
+     * @return SHA-1 hash of the File
+     */
     fun File.sha1() = HashUtils.sha1(this.readBytes())
 
+    /**
+     * Calculate a File's SHA-256 hash
+     *
+     * @return SHA-256 hash of the File
+     */
     fun File.sha256() = HashUtils.sha256(this.readBytes())
 
+    /**
+     * Calculate a File's SHA-512 hash
+     *
+     * @return SHA-512 hash of the File
+     */
     fun File.sha512() = HashUtils.sha512(this.readBytes())
 }

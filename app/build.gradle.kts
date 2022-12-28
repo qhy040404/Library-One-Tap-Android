16ef83a90336dfe9d723c8c7be34fa3bb4a35b3f
qhy040404 <qhy040404@163.com>
2022-12-28 22:56:04 +0800

    Release 3.8.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val baseVersionName = "3.8.8"
+val baseVersionName = "3.8.9"
 val commitsCount by lazy { "git rev-list --count HEAD".exec().toInt() }
 
 android {

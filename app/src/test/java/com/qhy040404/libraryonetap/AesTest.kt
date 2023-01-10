62e96d0f08a121116f64742a161cf2fa4574da22
qhy040404 <qhy040404@163.com>
2023-01-10 13:02:14 +0800

    Fix unit test

--- app/src/test/java/com/qhy040404/libraryonetap/AesTest.kt
+++ app/src/test/java/com/qhy040404/libraryonetap/AesTest.kt
@@ -8,11 +8,11 @@ class AesTest {
     @Test
     fun test() {
         Assert.assertEquals(
-            "/http/77726476706e69737468656265737421e4f2409f2f7e6c5c6b1cc7a99c406d3690/",
+            "https://webvpn.dlut.edu.cn/http/77726476706e69737468656265737421e4f2409f2f7e6c5c6b1cc7a99c406d3690/",
             WebVPNUtils.encryptUrl("http://teach.dlut.edu.cn/")
         )
         Assert.assertEquals(
-            "/https/77726476706e69737468656265737421e7e056d2233c7d44300d8db9d6562d/",
+            "https://webvpn.dlut.edu.cn/https/77726476706e69737468656265737421e7e056d2233c7d44300d8db9d6562d/",
             WebVPNUtils.encryptUrl("https://www.dlut.edu.cn/")
         )
     }

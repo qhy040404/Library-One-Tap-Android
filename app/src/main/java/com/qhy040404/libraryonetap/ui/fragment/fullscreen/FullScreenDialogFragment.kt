5295b25a5e1e6d3ffd6afb5924d4c7760860a75c
qhy040404 <qhy040404@163.com>
2023-01-16 16:20:37 +0800

    Simplify code

--- app/src/main/java/com/qhy040404/libraryonetap/ui/fragment/fullscreen/FullScreenDialogFragment.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/ui/fragment/fullscreen/FullScreenDialogFragment.kt
@@ -1,32 +1,48 @@
 package com.qhy040404.libraryonetap.ui.fragment.fullscreen
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.qhy040404.libraryonetap.LibraryOneTapApp
-import com.qhy040404.libraryonetap.base.BaseBindingDialogFragment
-import com.qhy040404.libraryonetap.databinding.FragmentFullscreenDialogBinding
+import com.qhy040404.libraryonetap.view.FullScreenDialogView
 
-class FullScreenDialogFragment(private val message: String) :
-    BaseBindingDialogFragment<FragmentFullscreenDialogBinding>() {
+class FullScreenDialogFragment(private val message: String) : DialogFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LibraryOneTapApp.instance?.addFragment(this)
     }
 
-    override fun onResume() {
-        super.onResume()
-        dialog?.window?.run {
-            attributes = attributes?.apply {
-                width = ViewGroup.LayoutParams.MATCH_PARENT
-                height = ViewGroup.LayoutParams.MATCH_PARENT
-                gravity = Gravity.BOTTOM
-            }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return FullScreenDialogView(requireContext()).apply {
+            detail.text = message
         }
     }
 
-    override fun initOnce() {
-        binding.pmDetail.text = message
+    override fun onResume() {
+        super.onResume()
+        dialog?.apply {
+            run {
+                setCanceledOnTouchOutside(isCancelable)
+                setCancelable(isCancelable)
+            }
+            window?.run {
+                attributes = attributes?.apply {
+                    width = ViewGroup.LayoutParams.MATCH_PARENT
+                    height = ViewGroup.LayoutParams.MATCH_PARENT
+                    gravity = Gravity.BOTTOM
+                }
+            }
+        }
     }
 
     override fun isCancelable() = false

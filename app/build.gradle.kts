65116db0a27a75bdad8fdf028805e1e06f130261
qhy040404 <qhy040404@163.com>
2023-04-14 18:39:44 +0800

    Prepare for 4.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val baseVersionName = "4.0.2"
+val baseVersionName = "4.0.3"
 val commitsCount by lazy { "git rev-list --count HEAD".exec().toInt() }
 
 android {

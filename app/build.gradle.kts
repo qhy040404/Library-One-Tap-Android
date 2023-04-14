6f046de0da2213a2658a6a1ac3bf7b5baf0f8fb1
qhy040404 <qhy040404@163.com>
2023-04-14 20:32:27 +0800

    Release 4.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val baseVersionName = "4.0.3"
+val baseVersionName = "4.1.0"
 val commitsCount by lazy { "git rev-list --count HEAD".exec().toInt() }
 
 android {

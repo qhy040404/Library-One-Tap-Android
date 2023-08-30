b9d7b837f095d1c8755ee4449bd82692aebc794c
qhy040404 <qhy040404@163.com>
2023-08-30 08:42:34 +0800

    Use commit hash to record migration

--- app/src/main/java/com/qhy040404/libraryonetap/utils/migration/BaseMigration.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/migration/BaseMigration.kt
@@ -2,13 +2,13 @@ package com.qhy040404.libraryonetap.utils.migration
 
 import jonathanfinerty.once.Once
 
-abstract class BaseMigration(private val versionName: String) {
+abstract class BaseMigration(private val commit: String) {
   protected abstract val reason: String
   protected abstract fun migrate()
 
   fun doMigration() {
-    if (Once.beenDone(versionName)) return
+    if (Once.beenDone(commit)) return
     migrate()
-    Once.markDone(versionName)
+    Once.markDone(commit)
   }
 }
--- app/src/main/java/com/qhy040404/libraryonetap/utils/migration/Migration.kt
+++ app/src/main/java/com/qhy040404/libraryonetap/utils/migration/Migration.kt
@@ -9,7 +9,7 @@ object Migration {
     MIGRATION_CHANGELOG.doMigration()
   }
 
-  private val MIGRATION_CHANGELOG = object : BaseMigration("4.2.8") {
+  private val MIGRATION_CHANGELOG = object : BaseMigration("5e22380b") {
     override val reason = "Local changelog was saved as HTML string, new version uses " +
       "MarkdownParser to parse changelog. As a result, an Exception will be thrown because " +
       "of MarkdownParser cannot parse a HTML string."
